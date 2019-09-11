e6a284d0eee29ee66d1f13bc0c291f4181192007
Tekiter <tekiter8@gmail.com>
2019-09-12 02:14:04 +0900

    Add bootstrap css

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import VueWait from 'vue-wait'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use(BootstrapVue)
 Vue.use(VueWait)
 
