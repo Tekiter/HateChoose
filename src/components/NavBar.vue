21dd5772e3adcc32fbd992b6fdb2e6c9e46d26c0
goseungduk <a457581@catholic.ac.kr>
2019-09-15 18:24:58 +0900

    add Roullet

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <b-navbar toggleable="lg">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="/">NavBar</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="#">Link</b-nav-item>
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item href="/roullet">Roullet</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
